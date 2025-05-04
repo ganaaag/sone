5d009e39fc39827fcfdfd7545740c9609ae0c90c
ganaaag <127759334+ganaaag@users.noreply.github.com>
2025-05-04 16:03:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     API_HASH = "d700b4abe3d023165894291b6fcbd3a0"
     ADMIN_ID = [1081310808]
     DB_URL = "mongodb+srv://navedmohammad2516:zu02cmOW6medghcJ@cluster0.cq1x5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    DB_NAME = os.environ.get("DB_NAME")
+    DB_NAME = os.environ.get("video_bot")
     TXT_LOG = -1002322908140
     HOST = "https://www.masterapi.tech"
 
