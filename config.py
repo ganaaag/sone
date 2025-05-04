339f33a7c586f255490ef28e6c62ba17ea988fc9
ganaaag <127759334+ganaaag@users.noreply.github.com>
2025-05-04 15:18:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = 26741932
-    API_HASH = "d9037cbe01f292eb440468c39c3dc41d"
-    ADMIN_ID = [7047543426]
+    API_ID = 25403301
+    API_HASH = "d700b4abe3d023165894291b6fcbd3a0"
+    ADMIN_ID = [1081310808]
     DB_URL = "mongodb+srv://navedmohammad2516:zu02cmOW6medghcJ@cluster0.cq1x5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     DB_NAME = os.environ.get("DB_NAME")
     TXT_LOG = -1002322908140
